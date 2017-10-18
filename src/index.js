2b0924a65b976c9a0f35ad5ce0e75e7e3c89b144
Gio d'Amelio <giodamelio@gmail.com>
2017-10-18 12:34:32 -0700

    Get basic jenkins info from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 const config = require('config');
+const Jenkins = require('jenkins');
 
-console.log(config.get('username'), config.get('token'));
+const jenkins = new Jenkins({
+  baseUrl: `https://${config.get('username')}:${config.get('token')}@ci.biztech-dev.com/`,
+  promisify: true,
+});
+
+jenkins.info().then((data) => {
+  console.log(data);
+}).catch((err) => {
+  console.error(err);
+});
